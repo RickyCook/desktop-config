6dd2751b5e1175fa6fde58d772f7dfe62d1b0081
FuzziBear <mail@fuzzibear.com>
2013-07-02 22:45:16 +1000

    Fixed copy.sh

--- copy.sh
+++ copy.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
-
-echo "This script is broken. It's being checked in for transport home" >2&
-exit 1
-
 DIR=$(cd $(dirname "$0"); pwd -L)
 
-mkdir -p "~/.config"
-mkdir -p "~/.xmonad"
-mkdir -p "~/bin"
+mkdir -p "$HOME/.config/tianbar"
+mkdir -p "$HOME/.xmonad"
 
 function do_link {
-	if [ -e "$2" ] && ! [ -L "$2" ] && ! [ -d "$2" ] && ! [ -f "$2" ]; then
+	if [ -e "$2" ] && ! [ -h "$2" ] && ! [ -d "$2" ] && ! [ -f "$2" ]; then
 		echo "File exists but unknown type: $2" >&2
 		return 1
 	fi
-	ex=0
-	[ -L "$2" ] && rm "$2" || ex=1
-	if [ $ex -ne 0 ]; then
-		echo "Link exists and can not remove $2" >&2
-		return 1
+	if [ -h "$2" ]; then 
+		rm "$2"
+		if [ $? -ne 0 ]; then
+			echo "Link exists and can not remove $2" >&2
+			return 1
+		fi
 	fi
-	[ -d "$2" ] || [ -f "$2" ] && mv "$2" "$2.old" || ex=1
-	if [ $ex -ne 0 ]; then
-		echo "Directory exists and can not move to backup $2" >&2
-		return 1
+	if [ -d "$2" ] || [ -f "$2" ]; then
+		mv "$2" "$2.old" || ex=1
+		if [ $? -ne 0 ]; then
+			echo "Directory exists and can not move to backup $2" >&2
+			return 1
+		fi
 	fi
-	ln -s "$1" "$2"
+
+	file=$(basename "$2")
+	dir=$(dirname "$2")
+	(cd "$dir"; ln -s "$1" "./$file")
 	if [ $? -ne 0 ]; then
 		echo "Could not link $0 -> $2" >&2
 		return 1
@@ -33,16 +34,26 @@ function do_link {
 	return 0
 }
 function do_link_all {
+	if ! [ -d "$1" ]; then
+		return 1
+	fi
+	if [ -e "$2" ] && ! [ -d "$2" ]; then
+		echo "Target exists but is not a directory: $2" >&2
+		return 1
+	fi
+	if ! [ -d "$2" ]; then
+		mkdir -p "$2"
+	fi
 	for f in $(ls "$1"); do
 		do_link "$1/$f" "$2/$f"
 	done
 }
 
-do_link "$DIR/.profile" "~/.profile"
-do_link "$DIR/xmonad.hs" "~/.xmonad/xmonad.hs"
-do_link "$DIR/tianbar" "~/.config/tianbar"
-do_link_all "$DIR/scripts" "~/bin"
-do_link_all "$DIR/xinit.d" "~/xinit.d"
+do_link "$DIR/.profile" "$HOME/.profile"
+do_link "$DIR/xmonad.hs" "$HOME/.xmonad/xmonad.hs"
+do_link "$DIR/tianbar" "$HOME/.config/tianbar"
+do_link_all "$DIR/scripts" "$HOME/bin"
+do_link_all "$DIR/xinit.d" "$HOME/xinit.d"
 
-do_link "/opt/Sublime Text 2/sublime_text" "~/bin"
-do_link "/opt/google/chrome/chrome" "~/bin"
+do_link "/opt/Sublime Text 2/sublime_text" "$HOME/bin/sublime_text"
+do_link "/opt/google/chrome/chrome" "$HOME/bin/chrome"
