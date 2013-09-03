b82c9a98df5477eb51e32460c07bbb0571f9cfd7
Ricky Cook <ricky@infoxchange.net.au>
2013-09-03 16:36:44 +1000

    Added a sublime title parser for cd to sublime dir

---
+++ scripts/sublime-cd.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use File::Basename;
+
+my $xwininfo = 'xwininfo -root -children|grep -Po \'".+?Sublime Text 2"\'|grep -o \'[^"].*[^"]\'';
+open(
+	my $info,
+	"$xwininfo|"
+) || die("Couldn't run: [$xwininfo]\n");
+
+my $first = <$info>;
+$first =~ s/ \([^)]+\) - Sublime Text 2//;
+$first =~ s/~/$ENV{HOME}/; # this is really naive
+
+print dirname($first);
