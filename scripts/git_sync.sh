b49c7710b90e5748bd9979090735353d573c1805
Ricky Cook <rcook@odecee.com.au>
2015-01-20 11:39:51 +1100

    Add git_sync.sh script

---
+++ scripts/git_sync.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+remote_name="$1"
+: ${remote_name:="origin"}
+
+git_root="$(cd "$(git rev-parse --show-toplevel)"; pwd)/"
+rsync_dest=$(git remote -v | grep "^origin\s.*\(push\)" | awk '{print $2}')
+
+echo "Remote name: $remote_name"
+echo "Repo root: $git_root"
+echo "Repo dest: $rsync_dest"
+
+rsync -zr --progress --exclude .git "$git_root" "$rsync_dest"
