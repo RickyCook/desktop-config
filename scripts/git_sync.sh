ec1a81e5c218e708c00f93c05e3b0bbb4bf231f6
Ricky Cook <rcook@odecee.com.au>
2015-01-23 11:13:18 +1100

    Fix git_sync remote override

--- scripts/git_sync.sh
+++ scripts/git_sync.sh
@@ -3,7 +3,7 @@ remote_name="$1"
 : ${remote_name:="origin"}
 
 git_root="$(cd "$(git rev-parse --show-toplevel)"; pwd)/"
-rsync_dest=$(git remote -v | grep "^origin\s.*\(push\)" | awk '{print $2}')
+rsync_dest=$(git remote -v | grep "^$remote_name\s.*\(push\)" | awk '{print $2}')
 
 echo "Remote name: $remote_name"
 echo "Repo root: $git_root"
